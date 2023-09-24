19ebd4eae9fecf57a4bb78881c3ce911ff7f8908
= <=>
2023-09-24 12:04:03 +0500

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,27 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import ideo from '../../public/ideogram.jpeg'
-import { Inter } from 'next/font/google'
-import Head from 'next/head'
-
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'AI in Healthcare',
-  description: 'Fatima Jinnah dental College artificial intelligence in healthcare course',
-}
+  title: "AI in Healthcare",
+  description:
+    "Fatima Jinnah dental College artificial intelligence in healthcare course",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href='/ideogram.jpeg' />
+        <link rel="icon" href="/ideogram.jpeg" />
       </Head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}  <Analytics /></body>
     </html>
-  )
+  );
 }
